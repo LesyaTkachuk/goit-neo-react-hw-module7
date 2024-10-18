7e8f62dbdc6e02934c81e965ef60069a6e643521
LesyaTkachuk <lesyalesya9876@gmail.com>
2024-10-18 11:38:45 +0300

    #2 fix delete contact

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
   useEffect(() => {
     dispatch(fetchContacts());
-  }, []);
+  }, [dispatch]);
 
   useEffect(() => {
     if (error) {
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -18,7 +18,7 @@ const contactsSlice = createSlice({
       })
       .addCase(deleteContact.fulfilled, (state, action) => {
         const index = state.items.findIndex(
-          (contact) => contact.id === action.payload
+          (contact) => contact.id === action.payload.id
         );
         state.items.splice(index, 1);
       })
